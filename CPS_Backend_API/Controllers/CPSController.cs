624a15f2eb339be037700cad2e2560b5ee7c7594
Ronaldo Rueda <rueda.ronaldo@hotmail.com>
2022-03-22 23:36:06 -0500

    Add project files.

---
+++ CPS_Backend_API/Controllers/CPSController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using CPS_Backend_API.Models;
+
+namespace CPS_Backend_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CPSController : Controller
+    {
+        private const string URL = "https://api.cps.edu/Calendar/cps/calendarslist";
+
+        //Method to call CPS API
+        public async Task<List<CalendarModel>> GetCalendars()
+        {
+            List<CalendarModel> calendars = new List<CalendarModel>();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                calendars = JsonConvert.DeserializeObject<List<CalendarModel>>(responseBody);
+
+                //Console.WriteLine("Calendar ID: {0}", temp.CalendarID);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            return calendars;
+        }
+
+        //API endpoint
+        [HttpGet]
+        public async Task<IEnumerable<CalendarModel>> GetAsync()
+        {
+            List<CalendarModel> calendars = await GetCalendars();
+
+            return calendars.ToArray();
+        }
+    }
+}
---
+++ CPS_Backend_API/Models/CalendarModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CPS_Backend_API.Models
+{
+    public class CalendarModel
+    {
+        public int CalendarID { get; set; }
+        public string CalendarName { get; set; }
+        public string html_Hex_ColorCode { get; set; }
+        public int TagID { get; set; }
+        public int ParentID { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+    }
+}
